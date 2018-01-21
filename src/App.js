9689d34833639aec2e22b60cd36bc902f5c62a04
keith <keith@raincityambience.com>
2018-01-21 05:46:04 -0800

    update WeatherResults component with loading logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import WeatherResults from './components/WeatherResults';
 
@@ -8,8 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          
-
+          <h1> Weather </h1>
         </header>
 
         <WeatherResults/>
--- src/components/WeatherResults.js
+++ src/components/WeatherResults.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import axios from 'axios';
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
+import '../weather-icons-master/css/weather-icons.css';
 import config from '../config.js';
 import moment from 'moment'
-
-
-
-
 
 
 class WeatherResults extends React.Component {
@@ -16,7 +13,7 @@ class WeatherResults extends React.Component {
     this.state = {
       weather: {},
       error: false,
-      loading: true,
+      loading: false,
       userZip: ''
     }
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -26,17 +23,19 @@ class WeatherResults extends React.Component {
 handleSubmit(event) {
     event.preventDefault()
     const that = this
-    axios.get('http://api.openweathermap.org/data/2.5/weather?zip=' + this.state.userZip +',us&APPID=' + config.apiKey +'&units=imperial')
-      .then(function(response) {
-        that.setState({weather: response.data, loading: false});
-        // const d = response.data
-        console.log(response.data, 'here is the response');
-      })
-      .catch(function(error) {
-        that.setState({error: true})
-        console.log(error, 'here is the error');
-      });
-
+    this.setState({loading: true})
+    setTimeout(function () {
+      axios.get('http://api.openweathermap.org/data/2.5/weather?zip=' + that.state.userZip +',us&APPID=' + config.apiKey +'&units=imperial')
+        .then(function(response) {
+          that.setState({weather: response.data, loading: false});
+          // const d = response.data
+          console.log(response.data, 'here is the response');
+        })
+        .catch(function(error) {
+          that.setState({error: true})
+          console.log(error, 'here is the error');
+        });
+    }, 3000);
 
     console.log(this.state.userZip)
 
@@ -46,15 +45,10 @@ handleSubmit(event) {
   this.setState({userZip: event.target.value});
 }
 
-
-
-
-  componentDidMount(){
-
-  }
-
   renderWeather(){
-    if(this.state.loading === false){
+    var loadingIcon = this.state.loading === true ? <i className="App-logo wi wi-yahoo-32" alt="logo" style={{fontSize: '40px'}}> </i> :  " "
+
+    if(this.state.loading === false && Object.keys(this.state.weather).length > 0){
       return (
         <div>
           <h2> {this.state.weather.name} </h2>
@@ -63,11 +57,16 @@ handleSubmit(event) {
           <h5>{this.state.weather.wind.speed + ' mph'}</h5>
           <h6>{'Sunrise: ' + moment.unix(this.state.weather.sys.sunrise).format('h:mm a')}</h6>
           <h6>{'Sunset: ' + moment.unix(this.state.weather.sys.sunset).format('h:mm a')}</h6>
-
-
-
         </div>
       )
+    } else if(this.state.loading === true) {
+      return (
+        <div>
+          {loadingIcon}
+        </div>
+      )
+    } else if(Object.keys(this.state.weather).length === 0){
+      return <h1>Search your zip code</h1>
     }
   }
 
@@ -76,22 +75,19 @@ handleSubmit(event) {
   }
 
   render() {
-    var loadingIcon = this.state.loading === true ? <img src={logo} className="App-logo" alt="logo" /> :  " "
 
     return (
       <div>
-        <h1> Weather </h1>
-        {this.renderWeather()}
         <form onSubmit={this.handleSubmit}>
-        <label>
-          Zip:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+          <label>
+            Zip:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
 
+        {this.renderWeather()}
 
-        {loadingIcon}
       </div>
     )
   }
